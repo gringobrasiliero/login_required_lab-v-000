78b8f79ee3902f4460bbbabc7da10137d3072253
Nolan Byrnes <nolanehs@gmail.com>
2018-03-16 10:10:37 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,9 @@
 class SessionsController < ApplicationController
 
 def create
-if session[:name] == nil || empty?
+if session[:name] == nil || session[:name].empty?
+  redirect_to login_path
+else
   session[:name] = params[:name]
   redirect_to '/'
 end
